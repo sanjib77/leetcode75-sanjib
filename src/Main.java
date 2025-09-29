8c5e6d5585061c27f15ae755715660950d2e1266
Sanjib <sanjib26spd@gmail.com>
2025-09-29 13:36:39 +0530

    Merge Strings Alternately1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MergeAlternately ma = new MergeAlternately();
+        String result = ma.mergeAlternately("abc", "pqr");
+        System.out.println(result);
+    }
+}
