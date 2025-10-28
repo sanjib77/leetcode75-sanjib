090cbcdcd48dae09af32aefe5ab1cf6597a82227
Sanjib <sanjib26spd@gmail.com>
2025-10-28 11:38:00 +0530

    feat: improved SinglyLinkedList Logic

--- practice/SInglyLinkedListBasics.py
+++ practice/SInglyLinkedListBasics.py
@@ -1,53 +1,74 @@
 class Node:
     def __init__(self, data):
+        """Initialize a new node with given data.
+        
+        Args:
+            data: The data to be stored in the node
+        """
         self.data = data
         self.next = None
+class SinglyLinkedList:
+    def __init__(self):
+        """Initialize an empty linked list."""
+        self.head = None
+    
+    def traverse(self):
+        """Traverse and print the linked list.
+        
+        Returns:
+            str: String representation of the linked list
+        """
+        result = []
+        curr_node = self.head
+        while curr_node:
+            result.append(str(curr_node.data))
+            curr_node = curr_node.next
+        return " -> ".join(result + ["None"])
 
-def traverse(head):
-    currNode = head
-    while currNode:
-        print(currNode.data, end = "-> ")
-        currNode = currNode.next
-    print("Null")
-
-node1 = Node(7)
-node2 = Node(11)
-node3 = Node(3)
-node4 = Node(2)
-node5 = Node(9)
-
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
-
-traverse(node1)
-
-#7-> 11-> 3-> 2-> 9-> Null
-#now we will try to delete a node in singly linked list
-def deleteNode(head,nodeTodelete):
-    if head is None:
-        print("Empty linked list")
-        return
-    currNode = head
-    while currNode:            
-        if currNode.next == nodeTodelete:
-            currNode.next = currNode.next.next
-            # del nodeTodelete
-            nodeTodelete = None
-
-            return head
-        currNode = currNode.next
-    return head
-
-
-#now we will delete 3rd node which has value 3
-head = node1
-print(head.data)
-deleteNode(head,node3)
-
-print(head.data)
-traverse(head)
-#7-> 11-> 2-> 9-> Null
-
-print(node3)
+    def delete_node(self, node_to_delete):
+        """Delete a specific node from the linked list.
+        
+        Args:
+            node_to_delete: The node to be deleted
+            
+        Returns:
+            bool: True if deletion was successful, False otherwise
+        """
+        if self.head is None:
+            return False
+            
+        if self.head == node_to_delete:
+            self.head = self.head.next
+            node_to_delete = None
+            return True
+            
+        curr_node = self.head
+        while curr_node.next:
+            if curr_node.next == node_to_delete:
+                curr_node.next = curr_node.next.next
+                node_to_delete = None
+                return True
+            curr_node = curr_node.next
+        return False
+#Example usage: 
+if __name__ == "__main__":
+    linked_list = SinglyLinkedList()
+    linked_list.head = Node(1)
+    second_node = Node(2)
+    third_node = Node(3)
+    
+    linked_list.head.next = second_node
+    second_node.next = third_node
+    
+    print("Original List:")
+    print(linked_list.traverse())
+    
+    linked_list.delete_node(second_node)
+    
+    print("After Deleting Node with value 2:")
+    print(linked_list.traverse())
+    print(second_node.data) # Should print 2, but node is deleted from list
+""" The reasoning behind printing second_node.data is to show that 
+the node object still exists in memory even after being removed from the linked list. 
+The deletion operation only removes the reference to the node from the linked list, 
+but does not delete the node object itself. Thus, we can still access its data attribute. """
