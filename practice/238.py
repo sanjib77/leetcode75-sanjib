416f12763b8e54e4515418527f3a6f0cfb0964ca
Sanjib <sanjib26spd@gmail.com>
2026-04-20 16:00:00 +0530

    Add solution for 238. Product of Array Except Self

---
+++ practice/238.py
@@ -0,0 +1,12 @@
+def productExceptSelf(nums):
+    n = len(nums)
+    res = [1] * n
+    left = 1
+    for i in range(n):
+        res[i] = left
+        left *= nums[i]
+    right = 1
+    for i in range(n - 1, -1, -1):
+        res[i] *= right
+        right *= nums[i]
+    return res
