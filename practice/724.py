25f2c7da3f37c17718f090c8e49cd10060ad53cf
Sanjib <sanjib26spd@gmail.com>
2026-04-26 15:30:00 +0530

    Add solution for 724. Find Pivot Index

---
+++ practice/724.py
@@ -0,0 +1,8 @@
+def pivotIndex(nums):
+    total = sum(nums)
+    left = 0
+    for i, x in enumerate(nums):
+        if left == total - left - x:
+            return i
+        left += x
+    return -1
