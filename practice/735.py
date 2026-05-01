2d7e867da0d1426666765ef7459c9cdfdab4e98c
Sanjib <sanjib26spd@gmail.com>
2026-05-01 12:30:00 +0530

    Add solution for 735. Asteroid Collision

---
+++ practice/735.py
@@ -0,0 +1,14 @@
+def asteroidCollision(asteroids):
+    stack = []
+    for a in asteroids:
+        while stack and a < 0 < stack[-1]:
+            if stack[-1] < -a:
+                stack.pop()
+            elif stack[-1] == -a:
+                stack.pop()
+                break
+            else:
+                break
+        else:
+            stack.append(a)
+    return stack
